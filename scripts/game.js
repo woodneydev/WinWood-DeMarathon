8cc77cd2df377d7cc6b0d75c75799ea6926fb926
Woodney Juste <woodneydev@gmail.com>
2023-07-21 01:18:38 -0400

    img js hide and show working

--- scripts/game.js
+++ scripts/game.js
@@ -3,8 +3,9 @@ const baseUrl = "https://www.themoviedb.org/t/p/w220_and_h330_face/";
 const form = document.querySelector(".game__interactive-form");
 console.log("this is the form el:", form)
 let randomNumber;
+let blurNumber = 40;
 
-const wrongPhrases = [
+const wrongPhrasesArr = [
     "Wrrrroooonnnnggggg!",
     "Come on man, you can do better!",
     "Are you even trying",
@@ -41,6 +42,7 @@ const populateMovies = () => {
         movieArray = [...data.results]
         console.log("movie Array after api call:", movieArray)
         loadOneMovie();
+        form.addEventListener("submit", handleSubmit)
     })
 }
 
@@ -48,18 +50,18 @@ const handleGuess = (guess, answer) => {
     const guesslength = guess.length
     const answerlength = answer.length
 
-    if (guesslength / answerlength <= .2) {
+    if (guesslength / answerlength <= .3) {
         console.log("guess was false");
-        return false
+        return false;
     }
     
     const guessArr = guess.split(" ");
     const guessCharArr = guess.split("");
 
-    const guessedRight = guessArr.forEach(guess => {
+    const guessedRight = guessArr.every(guess => {
         return answer.includes(guess)
     })
-    console.log(guessArr);
+    console.log(guessedRight);
 
     const guessedRightChars = guessCharArr.every(char => {
         return answer.includes(char)
@@ -67,8 +69,10 @@ const handleGuess = (guess, answer) => {
 
     if (answer.includes(guess) || guess.includes(answer) || guess === answer || guessedRight || guessedRightChars) {
         console.log("guess was correct")
+        return true;
     } else {
         console.log("guess was false");
+        return false;
     }
 }
 
@@ -78,8 +82,30 @@ const handleSubmit = event => {
     let guess = event.target.guess.value.toLowerCase().trim();
     let answer = movieArray[randomNumber].title.toLowerCase();
 
+    const imgEl = document.querySelector(".game__content-img");
+    console.log(imgEl)
+    const promptEl = document.querySelector(".game__interactive-prompt");
 
-    handleGuess(guess, answer);
+    //if wrong guess
+    if(!handleGuess(guess, answer)) {
+        if (blurNumber > 0) {
+            blurNumber -= 5
+            console.log(blurNumber)
+            imgEl.setAttribute("style", `filter: blur(${blurNumber}px)`)
+
+            
+            promptEl.innerText = wrongPhrasesArr[Math.round(Math.random() * (wrongPhrasesArr.length - 1))]
+        } 
+    } else {
+        movieArray.splice(randomNumber, 1)
+        console.log(movieArray);
+        promptEl.innerText = "Answer Correct"
+        setTimeout( () => {
+            promptEl.innerText = ""
+            loadOneMovie();
+        }, 3000)
+        
+    }
 
     
 }
@@ -87,4 +113,3 @@ const handleSubmit = event => {
 
 populateMovies();
 
-form.addEventListener("submit", handleSubmit)
